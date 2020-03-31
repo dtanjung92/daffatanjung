f801c049c5877877fc1900bf5b16bf4a50a8b4e2
dtanjung92 <62919005+dtanjung92@users.noreply.github.com>
2020-03-31 12:11:01 +0700

    Add files via upload

---
+++ pertemuan1/src/kelasutama.java
@@ -0,0 +1,22 @@
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author wew
+ */
+public class kelasutama {
+    public static void main(String[] args) {
+        JFrame futam2 = new JFrame();
+        futam2.setSize(300,200);
+        futam2.setTitle("PROGRAM PERTAMA");
+        futam2.setVisible(true);
+}
+}
